1e4d6dd703c1dba7acaf708d3ff239c8eb0bf2c1
Matheus de Paula <mthsdasil@gmail.com>
2022-08-02 21:36:24 -0300

    assertion and notations

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -11,8 +11,33 @@ use PHPUnit\Framework\TestCase;
  */
 class CartTest extends TestCase
 {
+    private $cart;
+    private $product;
 
-    public function test_if_cart_class_exists()
+    //Executa antes de cada teste
+    public function setUp(): void
+    {
+
+        $this->cart = new Cart();
+        $this->product = new Produto();
+    }
+    // executa após o fim de cada teste
+    public function tearDown(): void
+    {
+        unset($this->cart);
+        unset($this->product);
+    }
+
+    // public function test_if_cart_class_exists()
+    // {
+
+    //     $checkClass = class_exists('\\Code\\Cart');
+
+    //     $this->assertTrue($checkClass);
+    // }
+
+    // executa antes dos testes e deve retornar true ou false
+    public function assertPreConditions(): void
     {
 
         $checkClass = class_exists('\\Code\\Cart');
@@ -20,14 +45,18 @@ class CartTest extends TestCase
         $this->assertTrue($checkClass);
     }
 
+    public function assertPostConditions(): void
+    {
+    }
+
     public function test_it_should_be_able_add_product_in_cart()
     {
-        $product = new Produto();
+        $product =    $this->product;
         $product->setName('Teste 12');
         $product->setPrice(20);
         $product->setSlug('teste-12');
 
-        $cart = new Cart();
+        $cart =  $this->cart;
         $cart->addProduct($product);
 
         $this->assertIsArray($cart->getProducts());
@@ -36,48 +65,64 @@ class CartTest extends TestCase
 
     public function test_it_should_be_able_verify_correct_products_in_a_cart()
     {
-        $product = new Produto();
+        $product =    $this->product;
         $product->setName('Teste 12');
         $product->setPrice(20);
         $product->setSlug('teste-12');
 
-        $cart = new Cart();
+        $cart =  $this->cart;
         $cart->addProduct($product);
-        
+
         $this->assertEquals('Teste 12', $cart->getProducts()[0]->getName());
         $this->assertEquals(20, $cart->getProducts()[0]->getPrice());
         $this->assertEquals('teste-12', $cart->getProducts()[0]->getSlug());
     }
 
-    public function test_it_should_be_able_count_all_products_in_a_cart(){
-        $product = new Produto();
+    public function test_it_should_be_able_count_all_products_in_a_cart()
+    {
+        $product =    $this->product;
         $product->setName('Teste 12');
         $product->setPrice(20);
         $product->setSlug('teste-12');
 
 
-        $cart = new Cart();
+        $cart =  $this->cart;
         $cart->addProduct($product);
-        
+
         $this->assertEquals(1, $cart->countAllItems());
     }
 
-    public function test_it_should_be_able_sum_all_items_and_verify_value(){
-        $product = new Produto();
+    public function test_it_should_be_able_sum_all_items_and_verify_value()
+    {
+        $product =    $this->product;
         $product->setName('Teste 12');
         $product->setPrice(20);
         $product->setSlug('teste-12');
 
-        $product2 = new Produto();
+        $product2 =    $this->product;
         $product2->setName('Teste 12');
         $product2->setPrice(20);
         $product2->setSlug('teste-12');
 
 
-        $cart = new Cart();
+        $cart =  $this->cart;
         $cart->addProduct($product);
         $cart->addProduct($product2);
-        
+
         $this->assertEquals(40, $cart->sumAllPrices());
+    }
+
+    public function test_incomplete()
+    {
+        $this->markTestIncomplete('Teste não completo');
+    }
+/**
+ * @requires  PHP == 5.3
+ */
+    public function test_specific_php_version()
+    {
+        // if (PHP_VERSION != 5.3) $this->markTestSkipped('Apenas para versão abaixo de 7');
+
+        $this->assertTrue(true);
     }
 }
--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -6,34 +6,34 @@ use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
+
+    private $product;
+    public function setUp(): void
+    {
+        $this->product = new Produto();
+    }
     public function test_it_should_be_name_is_set()
     {
-        $product = new Produto();
+        $product =  $this->product;
         $product->setName('Teste 1');
-        
-        $this->assertEquals('Teste 1',$product->getName());
 
-        
+        $this->assertEquals('Teste 1', $product->getName());
     }
 
     public function test_it_should_be_price_is_set()
     {
-        $product = new Produto();
+        $product =  $this->product;
         $product->setPrice(20);
-        
-        $this->assertEquals(20,$product->getPrice());
 
-        
+        $this->assertEquals(20, $product->getPrice());
     }
 
 
     public function test_it_should_be_slug_is_set()
     {
-        $product = new Produto();
+        $product =  $this->product;
         $product->setSlug('teste-1');
-        
-        $this->assertEquals('teste-1',$product->getSlug());
 
-        
+        $this->assertEquals('teste-1', $product->getSlug());
     }
 }
